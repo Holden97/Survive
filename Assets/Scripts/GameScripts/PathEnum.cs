b0c5e865fe01cf1ca00fd9a5ede7a0b5e429d0f2
郭一波 <479276132@qq.com>
2021-02-19 20:00:26 +0800

    修改路径名称全部为小写

--- Assets/Scripts/GameScripts/PathEnum.cs
+++ Assets/Scripts/GameScripts/PathEnum.cs
@@ -8,18 +8,18 @@ public class PathEnum
     public static string ak_47_shoot = "AK_47_SHOOT";
     public static string dog_bark = "bark";
     public static string bug_hurt = "bug_hurt";
-    public static string button_Click = "button_Click";
-    public static string button_Hover = "button_Hover";
+    public static string button_click = "button_Click";
+    public static string button_hover = "button_Hover";
     public static string dog_death = "dog_death";
     public static string dog_death2 = "dog_death2";
     public static string dog_pant = "dog_pant";
     public static string hurt_man = "hurt_man";
-    public static string LauchFireBall = "LauchFireBall";
-    public static string noBullet2 = "noBullet2";
+    public static string fireball = "LauchFireBall";
+    public static string nobullet2 = "noBullet2";
     public static string nobullet3 = "nobullet3";
-    public static string PickUpCoin = "PickUpCoin";
-    public static string QSG92_RELOAD = "QSG92_RELOAD";
-    public static string QSG92_SHOOT = "QSG92_SHOOT";
+    public static string pickupcoin = "PickUpCoin";
+    public static string qsg92_reload = "QSG92_RELOAD";
+    public static string qsg92_shoot = "QSG92_SHOOT";
     public static string shotgun_reload = "shotgun_reload";
     public static string shotgun_shoot = "shotgun_shoot";
 }
